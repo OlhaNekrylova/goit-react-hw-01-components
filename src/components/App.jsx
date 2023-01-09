9e5679ff7f9d4a82e41ae5f7d229412d746ac623
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-01-09 21:05:24 +0200

    Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
+import Profile from './Profile';
+import profile from './src/user.json';
+
+export default function App (props) {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <Profile 
+      username={props.username}
+      tag={props.tag}
+      location={props.location}
+      avatar={props.avatar}
+      followers={props.stats.followers}
+      views={props.stats.views}
+      likes={props.stats.likes}
+    />
+  )
+}
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
