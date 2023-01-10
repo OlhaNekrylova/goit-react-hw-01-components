22eed9efd445639d4efd2ed47ab055763d1aba07
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-01-10 21:46:27 +0200

    FriendList

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import user from './user.json';
 import Statistics from './components/Statistics';
 import data from './data.json';
 
+import FriendList from './components/FriendList';
+import friends from './friends.json';
+
+
 export default function App () {
     return (
         <div>
@@ -17,6 +21,8 @@ export default function App () {
 
             <Statistics title="Upload stats" stats={data} />
             <Statistics stats={data} />
+
+            <FriendList friends={friends} />;
 
             
         </div>   
---
+++ src/components/FriendList.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+const FriendList = ({friends}) => {
+    return (
+        <ul class="friend-list">
+            {friends.map(({avatar, name, isOnline, id}) => (
+                <li 
+                    class="item"
+                    key={id}
+                >
+                    <span class="status">{isOnline}</span>
+                    <img class="avatar" src={avatar} alt="User avatar" width="48" />
+                    <p class="name">{name}</p>
+                </li>
+            )
+            )}
+        </ul>
+    )
+}
