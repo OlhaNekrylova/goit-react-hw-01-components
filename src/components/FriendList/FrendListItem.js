6989d80943340968d67f77f75fe1ecbbd9a3eb54
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-01-13 22:41:52 +0200

    css.status in FrendListItem

--- src/components/FriendList/FrendListItem.js
+++ src/components/FriendList/FrendListItem.js
@@ -4,7 +4,7 @@ import css from './FriendList.module.css';
 const FriendListItem = ({avatar, name, isOnline}) => {
     return (
         <li className={css.item}>
-            <span classNames={css.status}>{isOnline}</span>
+            <span classNames={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
             <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={css.name}>{name}</p>
         </li>
