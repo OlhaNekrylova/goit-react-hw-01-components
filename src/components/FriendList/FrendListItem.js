786abffe0d85e73b2e9a553509ffe2789e861940
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-01-15 00:23:06 +0200

    isOnline

--- src/components/FriendList/FrendListItem.js
+++ src/components/FriendList/FrendListItem.js
@@ -4,7 +4,7 @@ import css from './FriendList.module.css';
 const FriendListItem = ({avatar, name, isOnline}) => {
     return (
         <li className={css.item}>
-            <span classNames={isOnline ? css.statusOnline : css.statusOffline}>{isOnline}</span>
+            <span classNames={css[isOnline] ? css.statusOnline : css.statusOffline}>{isOnline}</span>
             <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={css.name}>{name}</p>
         </li>
